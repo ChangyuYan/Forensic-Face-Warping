224b99af18a5f441599b9ff1c33c089a1c5857a0
ChangyuYan <changyannnnnn@gmail.com>
2021-08-24 03:16:10 -0700

    succesfully swapped one triangle of face to the one of template

--- face_swapping.py
+++ face_swapping.py
@@ -39,7 +39,7 @@ for face in faces1:
     points = np.array(landmark_points, np.int32)
     convex_hull = cv2.convexHull(points)
 
-    cv2.polylines(IMG1, [convex_hull], True, (255, 0, 0), 3)
+    # cv2.polylines(IMG1, [convex_hull], True, (255, 0, 0), 3)
     cv2.fillConvexPoly(MASK, convex_hull, 255)
 
     face_image_1 = cv2.bitwise_and(IMG1, IMG1, mask=MASK)
@@ -65,39 +65,86 @@ for face in faces1:
         if index_pt1 is not None and index_pt2 is not None and index_pt3 is not None:
             indices_triangles.append([index_pt1, index_pt2, index_pt3])
 
-        cv2.line(IMG1, pt1, pt2, (0, 0, 255), 2)
-        cv2.line(IMG1, pt2, pt3, (0, 0, 255), 2)
-        cv2.line(IMG1, pt3, pt1, (0, 0, 255), 2)
-
 
 # TODO: A lot of repeats here; optimize this
 faces2 = detector(TEMPLATE_GRAY)
 
+
 for face in faces2:
     landmarks = predictor(TEMPLATE_GRAY, face)
-    landmark_points = []
+    landmark_points2 = []
     for n in range(LANDMARK_COUNT):
         x = landmarks.part(n).x
         y = landmarks.part(n).y
-        landmark_points.append((x, y))
+        landmark_points2.append((x, y))
 
-        cv2.circle(TEMPLATE, (x, y), 3, (0, 255, 0), -1)
+        # cv2.circle(TEMPLATE, (x, y), 3, (0, 255, 0), -1)
 
-# Triangulation of the second face, from the first face Delaunay Triangulation
-
+# Triangulation of both faces
 for triangle_index in indices_triangles:
-    pt1 = landmark_points[triangle_index[0]]
-    pt2 = landmark_points[triangle_index[1]]
-    pt3 = landmark_points[triangle_index[2]]
+    # Triangulation of the first face
+    tr1_pt1 = landmark_points[triangle_index[0]]
+    tr1_pt2 = landmark_points[triangle_index[1]]
+    tr1_pt3 = landmark_points[triangle_index[2]]
 
-    cv2.line(TEMPLATE, pt1, pt2, (0, 0, 255), 2)
-    cv2.line(TEMPLATE, pt2, pt3, (0, 0, 255), 2)
-    cv2.line(TEMPLATE, pt3, pt1, (0, 0, 255), 2)
+    triangle1 = np.array([tr1_pt1, tr1_pt2, tr1_pt3], np.int32)
 
+    rect1 = cv2.boundingRect(triangle1)
+    (x, y, w, h) = rect1
+    # cv2.rectangle(IMG1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cropped_triangle = IMG1[y: y + h, x: x + w]
+    cropped_tr1_mask = np.zeros((h, w), np.uint8)
+
+    points1 = np.array([[tr1_pt1[0] - x, tr1_pt1[1] - y],
+                        [tr1_pt2[0] - x, tr1_pt2[1] - y],
+                        [tr1_pt3[0] - x, tr1_pt3[1] - y]])
+
+    cv2.fillConvexPoly(cropped_tr1_mask, points1, 255)
+    cropped_triangle = cv2.bitwise_and(cropped_triangle, cropped_triangle, mask=cropped_tr1_mask)
+
+    cv2.line(IMG1, tr1_pt1, tr1_pt2, (0, 0, 255), 2)
+    cv2.line(IMG1, tr1_pt2, tr1_pt3, (0, 0, 255), 2)
+    cv2.line(IMG1, tr1_pt3, tr1_pt1, (0, 0, 255), 2)
+
+    # Triangulation of the second face, from the first face Delaunay Triangulation
+    tr2_pt1 = landmark_points2[triangle_index[0]]
+    tr2_pt2 = landmark_points2[triangle_index[1]]
+    tr2_pt3 = landmark_points2[triangle_index[2]]
+
+    triangle2 = np.array([tr2_pt1, tr2_pt2, tr2_pt3], np.int32)
+
+    rect2 = cv2.boundingRect(triangle2)
+    (x, y, w, h) = rect2
+
+    cropped_triangle2 = TEMPLATE[y: y + h, x: x + w]
+
+    cropped_tr2_mask = np.zeros((h, w), np.uint8)
+
+    points2 = np.array([[tr2_pt1[0] - x, tr2_pt1[1] - y],
+                        [tr2_pt2[0] - x, tr2_pt2[1] - y],
+                        [tr2_pt3[0] - x, tr2_pt3[1] - y]])
+
+    cv2.fillConvexPoly(cropped_tr2_mask, points2, 255)
+    cropped_triangle2 = cv2.bitwise_and(cropped_triangle2, cropped_triangle2, mask=cropped_tr2_mask)
+
+    cv2.line(TEMPLATE, tr2_pt1, tr2_pt2, (0, 0, 255), 2)
+    cv2.line(TEMPLATE, tr2_pt2, tr2_pt3, (0, 0, 255), 2)
+    cv2.line(TEMPLATE, tr2_pt3, tr2_pt1, (0, 0, 255), 2)
+
+    # Warp Triangles
+
+    points1 = np.float32(points1)
+    points2 = np.float32(points2)
+
+    M = cv2.getAffineTransform(points1, points2)
+    warp_triangle = cv2.warpAffine(cropped_triangle, M, (w, h))
+
+    break
 
 cv2.imshow("Image 1", IMG1)
-cv2.imshow("Face Image 1", face_image_1)
 cv2.imshow("Template", TEMPLATE)
-cv2.imshow("Mask", MASK)
+cv2.imshow("Cropped Triangle 1", cropped_triangle)
+cv2.imshow("Cropped Triangle 2", cropped_triangle2)
+cv2.imshow("Warped Triangle", warp_triangle)
 cv2.waitKey(0)
 cv2.destroyWindow(WINDOW_NAME)
