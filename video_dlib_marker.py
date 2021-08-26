3d91e120bfc1159264a9a37005873dd2bd52911d
ChangyuYan <changyannnnnn@gmail.com>
2021-08-26 04:16:34 -0700

    updated script

--- video_dlib_marker.py
+++ video_dlib_marker.py
@@ -4,10 +4,13 @@ cap = cv2.VideoCapture("./data/Deepfakes/000_003.mp4")
 
 count = 0
 while cap.isOpened():
-    ret, frame = cap.read()
-    cv2.imshow('window-name', frame)
-    cv2.imwrite("frame%d.jpg" % count, frame)
-    count = count + 1
+    success, frame = cap.read()
+    if success:
+        cv2.imshow('window-name', frame)
+        # cv2.imwrite("frame%d.jpg" % count, frame)
+        count = count + 1
+    else:
+        break
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
